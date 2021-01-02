a9ae445df001ecf4bb536240223c3e2e2e3043c2
Tori <tori.bosler@hotmail.com>
2021-01-02 11:39:10 -0800

    Commit Assignment Portal Page

---
+++ projects/mtnbike/js/currentdate.js
@@ -0,0 +1,3 @@
+// to local date
+const options = {weekday: 'long', day: 'numeric', month: 'long', year:'numeric'};
+document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
