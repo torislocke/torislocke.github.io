fb98828d4247e7bbfd4948273f2796091a529787
Tori <tori.bosler@hotmail.com>
2021-02-01 10:53:18 -0800

    lesson 5 draft

---
+++ lesson5/js/currentDates.js
@@ -0,0 +1,11 @@
+// JavaScript to maintain the current year information of copyright in footer
+let currentYear = new Date();
+document.getElementById('currentYear').innerHTML = currentYear.getFullYear();
+
+//footer script to display current date without time
+
+let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' };
+let today = new Date();
+document.getElementById('currentDate').innerHTML = today.toLocaleDateString('en-AU', options);
+
+// end footer script to display current date and time
