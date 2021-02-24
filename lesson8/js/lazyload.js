f271b561ebc40f5240308dee8710153ef80eca25
Tori <tori.bosler@hotmail.com>
2021-02-24 07:56:39 -0800

    Storm Center

---
+++ lesson8/js/lazyload.js
@@ -0,0 +1,35 @@
+// Another version of using the IntersectionObserver API
+//Retrieve images with data - src attribute
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+
+// Establish optional paramenters for the InsectionalObserver
+const imgOptions = {
+	threshold: 1, // delays the load the higher the number
+	rootMargin: '0px 0px 50px 0px',
+};
+
+const loadImages = (image) => {
+	image.setAttribute('src', image.getAttribute('data-src'));
+	image.onload = () => {
+		image.removeAttribute('data-src');
+	};
+};
+
+//verify if intersection Observer is supported
+if ('IntersectionObserver' in window) {
+	const observer = new IntersectionObserver((items, observer) => {
+		items.forEach((item) => {
+			if (item.isIntersecting) {
+				loadImages(item.target);
+				observer.unobserve(item.target);
+			}
+		});
+	});
+	imagesToLoad.forEach((img) => {
+		observer.observe(img);
+	});
+} else {
+	imagesToLoad.forEach((img) => {
+		loadImages(img);
+	});
+}
